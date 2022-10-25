b5672d884b88a811e3e85159b65c763d45edec93
Maxim <68609996+squareGTS@users.noreply.github.com>
2022-10-25 22:14:47 +0300

    Setting Up the User Interface

---
+++ CameraFilterRx/CameraFilterRx/PhotosCollectionViewController.swift
@@ -0,0 +1,12 @@
+//
+//  PhotosCollectionViewController.swift
+//  CameraFilterRx
+//
+//  Created by Maxim Bekmetov on 25.10.2022.
+//
+
+import UIKit
+
+class PhotosCollectionViewController: UICollectionViewController {
+
+}
