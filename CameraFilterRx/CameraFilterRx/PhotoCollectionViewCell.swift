17e25408d9a7510f26d3d555ad90c8f557ed8614
Maxim <68609996+squareGTS@users.noreply.github.com>
2022-10-26 10:30:27 +0300

    Displaying Photos from Photo Library

---
+++ CameraFilterRx/CameraFilterRx/PhotoCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoCollectionViewCell.swift
+//  CameraFilterRx
+//
+//  Created by Maxim Bekmetov on 26.10.2022.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var photoImageView: UIImageView!
+}
--- CameraFilterRx/CameraFilterRx/PhotosCollectionViewController.swift
+++ CameraFilterRx/CameraFilterRx/PhotosCollectionViewController.swift
@@ -6,7 +6,54 @@
 //
 
 import UIKit
+import Photos
 
 class PhotosCollectionViewController: UICollectionViewController {
 
+    private var images = [PHAsset]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        populatePhotos()
+    }
+
+    private func populatePhotos() {
+
+        PHPhotoLibrary.requestAuthorization { [weak self] status in
+
+            if status == .authorized {
+
+                let assests = PHAsset.fetchAssets(with: PHAssetMediaType.image, options: nil)
+                assests.enumerateObjects { (object, count, stop) in
+                    self?.images.append(object)
+                }
+                self?.images.reversed()
+                DispatchQueue.main.async {
+                    self?.collectionView.reloadData()
+                }
+            }
+        }
+    }
+
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        1
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        self.images.count
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoCollectionViewCell", for: indexPath) as? PhotoCollectionViewCell else { fatalError("UICollectionViewCell is not found") }
+
+        let asset = self.images[indexPath.row]
+        let manager = PHImageManager.default()
+        manager.requestImage(for: asset, targetSize: CGSize(width: 100, height: 100), contentMode: .aspectFit, options: nil) { image, _ in
+
+            DispatchQueue.main.async {
+                cell.photoImageView.image = image
+            }
+        }
+        return cell
+    }
 }
